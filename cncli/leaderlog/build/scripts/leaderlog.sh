a0ca08d8133ef99dab6894fca6ea219690058413
Reza <28660160+rezarajan@users.noreply.github.com>
2023-06-23 11:53:30 -0400

    feat(cncli-sendslots): update Dockerfile and scripts to support v8.1.1

--- cncli/leaderlog/build/scripts/leaderlog.sh
+++ cncli/leaderlog/build/scripts/leaderlog.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 SNAPSHOT=$(cardano-cli query stake-snapshot --stake-pool-id $(cat ${CNODE_HOME}/priv/pool/pool.id) --mainnet)
-POOL_STAKE=$(echo "$SNAPSHOT" | grep -oP '(?<=    "poolStakeMark": )\d+(?=,?)')
-ACTIVE_STAKE=$(echo "$SNAPSHOT" | grep -oP '(?<=    "activeStakeMark": )\d+(?=,?)')
+POOL_STAKE=$(echo $SNAPSHOT | jq '.pools."4e46972f7896ca3361efe216d70f65dbeaa926a693219d74fca43660".stakeMark')
+ACTIVE_STAKE=$(echo $SNAPSHOT | jq .total.stakeMark)
 MOCHI=`cncli leaderlog \
     --db ${CNODE_HOME}/cncli/db/cncli.db \
     --pool-id $(cat ${CNODE_HOME}/priv/pool/pool.id) \
     --pool-vrf-skey ${CNODE_HOME}/priv/pool/keys/vrf.skey \
     --byron-genesis ${CNODE_HOME}/configs/byron-genesis.json \
     --shelley-genesis ${CNODE_HOME}/configs/shelley-genesis.json \
-    --pool-stake $POOL_STAKE \
-    --active-stake $ACTIVE_STAKE \
+    --pool-stake 5704575471461 \
+    --active-stake 22735339826891192 \
     --consensus praos \
-    --ledger-set current`
+    --ledger-set next`
 
 EPOCH=`jq .epoch <<< $MOCHI`
 
@@ -23,3 +23,4 @@ PERFORMANCE=`jq .maxPerformance <<< $MOCHI`
 echo "MOCHI" | tee -a ${LOG_DIR}/leaderlog.log
 echo "\`Epoch $EPOCH\` 🧙🔮:" | tee -a ${LOG_DIR}/leaderlog.log
 echo "\`MOCHI  - $SLOTS \`🎰\`,  $PERFORMANCE% \`🍀max, \`$IDEAL\` 🧱ideal" | tee -a ${LOG_DIR}/leaderlog.log
+echo $MOCHI | tee -a ${LOG_DIR}/leaderlog.log
